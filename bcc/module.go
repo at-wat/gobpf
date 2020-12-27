5ae55436c3f7bc8fa1e1320fb424c1bbd7e701d4
Gui774ume <guillaume.fournier@datadoghq.com>
2020-12-27 22:18:48 +0100

    comment out bpf_attach_uprobe

--- bcc/module.go
+++ bcc/module.go
@@ -258,14 +258,17 @@ func (bpf *Module) attachProbe(evName string, attachType uint32, fnName string, 
 func (bpf *Module) attachUProbe(evName string, attachType uint32, path string, addr uint64, fd, pid int) error {
 	evNameCS := C.CString(evName)
 	binaryPathCS := C.CString(path)
-	res, err := C.bpf_attach_uprobe(C.int(fd), attachType, evNameCS, binaryPathCS, (C.uint64_t)(addr), (C.pid_t)(pid))
+	// In bcc 0.17, a new function parameter was added which breaks compatibility with this version of gobpf. Since we
+	// do not currently use DataDog/gobpf to load uprobes and will always use DataDog/ebpf in future project, this piece
+	// of code is breaking compatibility with new installs of bcc for no reason.
+	// res, err := C.bpf_attach_uprobe(C.int(fd), attachType, evNameCS, binaryPathCS, (C.uint64_t)(addr), (C.pid_t)(pid))
 	C.free(unsafe.Pointer(evNameCS))
 	C.free(unsafe.Pointer(binaryPathCS))
 
-	if res < 0 {
-		return fmt.Errorf("failed to attach BPF uprobe: %v", err)
-	}
-	bpf.uprobes[evName] = int(res)
+	//if res < 0 {
+	//	return fmt.Errorf("failed to attach BPF uprobe: %v", err)
+	//}
+	//bpf.uprobes[evName] = int(res)
 	return nil
 }
 
